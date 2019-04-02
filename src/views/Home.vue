25f6965b436e6057cce912ea9645b7096091854b
王一杨 <wangyiyang@163.com>
2019-04-02 17:39:49 +0800

    vuex-class

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <div>{{myName}}</div>
+    <div>{{name}}</div>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
@@ -9,15 +9,15 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-import { mapState } from 'vuex'
+import { State, Getter, Mutation, Action } from 'vuex-class'
+
+
 @Component({
   components: {
     HelloWorld,
   },
 })
 export default class Home extends Vue {
-  get myName(): string {
-    return this.$store.state.name
-  }
+  @State name!: string
 }
 </script>
