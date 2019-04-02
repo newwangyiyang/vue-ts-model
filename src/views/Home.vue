0a9b8d2c47d36c86ba4d0631ece4ba12ec54ba55
王一杨 <wangyiyang@163.com>
2019-04-02 17:31:55 +0800

    typescript-vue-demo

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <div>{{myName}}</div>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { mapState } from 'vuex'
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {
+  get myName(): string {
+    return this.$store.state.name
+  }
+}
+</script>
